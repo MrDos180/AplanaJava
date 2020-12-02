55929d25f3caeddbc5c0771958fd33764888e91e
MrDos180 <55959671+MrDos180@users.noreply.github.com>
2020-12-02 23:09:42 +0300

    задание 3

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,12 +1,13 @@
 public class Arrays {
     public static void main(String[] args) {
         int[] array = {1,2,3,4,5};
-        int first= array[array.length-5];
+        int first= array[0];
         int last= array[array.length-1];
+        int mid= array[array.length/2];
         array[0]=last;
         array[4]=first;
 
-        System.out.println(array[0]+array[2]);
+        System.out.println(array[0]+mid);
     }
 }
 
