11b059e2893953371e83d95e5e3ddcbb4d558de6
MrDos180 <55959671+MrDos180@users.noreply.github.com>
2020-12-07 22:11:25 +0300

    Create Task4.java
    задание 4

---
+++ src/Task4.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Task4 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+        double mid = (double) (a + b + c) / 3;
+        System.out.println(mid);
+        double d = Math.floor(mid / 2);
+        if (d > 3) {
+            System.out.println("Программа выполнена корректно");
+        }
+    }
+}
