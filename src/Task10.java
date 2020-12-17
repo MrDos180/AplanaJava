a28a748c1460e6c9ea105427d1a510c613ae9545
MrDos180 <55959671+MrDos180@users.noreply.github.com>
2020-12-17 17:44:38 +0300

    Create Task10.java
    задание 10

---
+++ src/Task10.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Task10 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Введите количесво строк");
+        int a = sc.nextInt();
+        System.out.println("Введите количество столбцов");
+        int b = sc.nextInt();
+        int[][] array = new int[a][b];
+        for(int i = 0;i<a;i++){
+            for(int q = 0;q<b;q++){
+                System.out.println("Введите [" + i +", " + q + "] элемент матрицы");
+                array[i][q]=sc.nextInt();
+            }
+        }
+
+        for(int i=0;i<b;i++){
+            System.out.println(array[0][i]*3);
+        }
+
+    }
+}
