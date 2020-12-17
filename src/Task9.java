7af45080c8a6bcf66461c3075aaf06e056c7305b
MrDos180 <55959671+MrDos180@users.noreply.github.com>
2020-12-17 18:58:29 +0300

    ctrl+alt+l

--- src/Task9.java
+++ src/Task9.java
@@ -5,17 +5,17 @@ public class Task9 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите размер массива");
         int a = sc.nextInt();
-        double[] array= new double[a];
+        double[] array = new double[a];
         double sum = 0;
-        for(int i=0;i < a;i++){
-            System.out.println("Введите "+i+" элемент массива в формате n,n");
-            array[i]=sc.nextDouble();
-            sum+=array[i];
+        for (int i = 0; i < a; i++) {
+            System.out.println("Введите " + i + " элемент массива в формате n,n");
+            array[i] = sc.nextDouble();
+            sum += array[i];
         }
-        double mid=sum/a;
+        double mid = sum / a;
 
-        for(int i=0;i < a;i++){
-            System.out.println(array[i]*mid);
+        for (int i = 0; i < a; i++) {
+            System.out.println(array[i] * mid);
         }
     }
 }
