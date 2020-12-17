02310ae7f53faa4b6477bb80660dc6bf39b37b84
MrDos180 <55959671+MrDos180@users.noreply.github.com>
2020-12-17 17:44:30 +0300

    Create Task9.java
    задание 9

---
+++ src/Task9.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Task9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите размер массива");
+        int a = sc.nextInt();
+        double[] array= new double[a];
+        double sum = 0;
+        for(int i=0;i < a;i++){
+            System.out.println("Введите "+i+" элемент массива в формате n,n");
+            array[i]=sc.nextDouble();
+            sum+=array[i];
+        }
+        double mid=sum/a;
+
+        for(int i=0;i < a;i++){
+            System.out.println(array[i]*mid);
+        }
+    }
+}
